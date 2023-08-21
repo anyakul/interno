602fc022cd42a2f40ca89cef06a96f99926977ba
anyakulikova111@ <anyakulikova111@gmail.com>
2023-08-21 12:37:11 +0300

    first commit

--- src/js/modules/header.js
+++
@@ -1,8 +0,0 @@
-export function header() {
-  let headerButton = document.querySelector(".header-menu-button")
-  let headerContainer = document.querySelector(".header-container")
-
-  headerButton.addEventListener('click', function (e) {
-    headerContainer.classList.toggle("header-container-active")
-  })
-}
